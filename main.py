97c8d25844cbaddadf6a9fb8eb705eef581cd256
Vegard Bønes <vegardb@met.no>
2025-08-12 11:55:04 +0200

    add running version

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from bris-fiab!")
+from anemoi.inference.config.run import RunConfiguration
+from anemoi.inference.runners.default import DefaultRunner
 
 
-if __name__ == "__main__":
-    main()
+def run():
+    config = RunConfiguration.load('config.yaml')
+
+    runner = DefaultRunner(config)
+
+    import torch
+    if torch.cuda.is_available():
+        runner.device = "cuda"
+    elif torch.backends.mps.is_available():
+        runner.device = "mps"
+    else:
+        runner.device = "cpu"
+
+    runner.execute()
+
+
+if __name__ == '__main__':
+    run()
