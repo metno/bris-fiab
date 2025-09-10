0d24097a48772f1230a10ac3f47057c85701d120
Vegard Bønes <vegardb@met.no>
2025-09-10 09:20:16 +0200

    add option to embed reprojected model elevation into checkpoint

--- update_checkpoint.py
+++ update_checkpoint.py
@@ -6,16 +6,18 @@ from bris_fiab.checkpoint import graph
 @click.option('--topography-file', type=click.Path(exists=True))
 @click.option('--original-checkpoint', type=click.Path(exists=True))
 @click.option('--create-checkpoint', type=click.Path())
-@click.option('--save-graph-to', type=click.Path(), default='', help='Path to save the graph file.')
+@click.option('--add-model-elevation', is_flag=True, default=False, help='Download and add model elevation data to the checkpoint.')
+@click.option('--save-graph-to', type=click.Path(), default='', help='Save the graph file to this path in addition to integrating it into the checkpoint.')
 @click.option('--save-latlon', type=bool, default=False, help='Whether to save the latitude/longitude to files.')
 @click.option('--lam-resolution', type=int, default=10)
 @click.option('--global-resolution', type=int, default=7)
 @click.option('--margin-radius-km', type=int, default=11)
-def cli(topography_file: str, original_checkpoint: str, create_checkpoint: str, save_graph_to: str, save_latlon: bool, lam_resolution: int, global_resolution: int, margin_radius_km: int):
+def cli(topography_file: str, original_checkpoint: str, create_checkpoint: str, add_model_elevation: bool, save_graph_to: str, save_latlon: bool, lam_resolution: int, global_resolution: int, margin_radius_km: int):
     graph.run(
         topography_file=topography_file,
         original_checkpoint=original_checkpoint,
         new_checkpoint=create_checkpoint,
+        add_model_elevation=add_model_elevation,
         save_graph_to=save_graph_to,
         save_latlon=save_latlon,
         graph_config=graph.GraphConfig(
