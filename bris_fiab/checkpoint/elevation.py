0ee4b17d6070059744383bc4a1d2d3ef7f7a7fcf
Vegard Bønes <vegardb@met.no>
2025-09-18 11:04:17 +0200

    use relative date when requesting z for checkpoint

--- bris_fiab/checkpoint/elevation.py
+++ bris_fiab/checkpoint/elevation.py
@@ -12,7 +12,7 @@ def get_model_elevation(lats: np.ndarray, lons: np.ndarray) -> np.ndarray:
     min_lon = np.floor(np.min(lons) * 10) / 10
     request = {
         'area': [max_lat, min_lon, min_lat, max_lon],
-        'date': ['2025-03-05'],
+        'date': -10,
         'expver': '0001',
         'grid': '0.1/0.1',
         'levtype': 'sfc',
