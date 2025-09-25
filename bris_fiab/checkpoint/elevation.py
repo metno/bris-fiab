50173264ed6e962adadd08e7471f868f7a70076a
Børge Moe <borge.moe@met.no>
2025-09-25 14:20:08 +0200

    WIP: download and create elevation field

--- bris_fiab/checkpoint/elevation.py
+++ bris_fiab/checkpoint/elevation.py
@@ -4,6 +4,7 @@ import metpy.calc
 from metpy.units import units
 from bris_fiab.anemoi_plugins.inference.downscale.downscale import Topography, downscaler
 
+
 def get_model_elevation(lats: np.ndarray, lons: np.ndarray) -> np.ndarray:
     '''Fetch model elevation from ECMWF MARS, interpolated to the given latitudes and longitudes.'''
     max_lat = np.ceil(np.max(lats) * 10) / 10
@@ -14,7 +15,7 @@ def get_model_elevation(lats: np.ndarray, lons: np.ndarray) -> np.ndarray:
         'area': [max_lat, min_lon, min_lat, max_lon],
         'date': -10,
         'expver': '0001',
-        'grid': '0.1/0.1',
+        'grid': '0.05/0.05',
         'levtype': 'sfc',
         'param': ['z'],
         'step': 0,
@@ -24,8 +25,37 @@ def get_model_elevation(lats: np.ndarray, lons: np.ndarray) -> np.ndarray:
     raw_data = ekd.from_source('mars', request)[0]
 
     geopotential = units.Quantity(raw_data.to_numpy(), 'm^2/s^2')
+    print(f'Geopotential min/max: {geopotential.min()}/{geopotential.max()}')
     height = metpy.calc.geopotential_to_height(geopotential)
 
     latlons = raw_data.to_latlon()
     return downscaler(latlons['lon'], latlons['lat'], lons, lats)(height).astype('int16')
 
+
+def get_model_elevation_mars_grid(area_latlon: tuple[float, float, float, float, float]) -> np.ndarray:
+    """The function use earthkit.data to download model elevation for the specified area from Mars.
+    area_latlon: (north, west, south, east, resolution)
+    returns: elevation
+    """
+
+    # resolution is area_latlon[4]
+    # return lat, lon arrays
+    area = [area_latlon[0], area_latlon[1], area_latlon[2], area_latlon[3]]
+    ds = ekd.from_source(  # type: ignore
+        'mars',
+        {
+            'AREA': area,
+            'GRID': f"{area_latlon[4]}/{area_latlon[4]}",
+            'param': 'z',
+            'date': -34,
+            'stream': 'oper',
+            'type': 'an',
+            'levtype': 'sfc',
+        }
+    )
+    geopotential = units.Quantity(ds[0].to_numpy(), 'm^2/s^2')
+    height = metpy.calc.geopotential_to_height(geopotential)
+    print(
+        f"get_model_elevation_mars_grid: Downloaded elevation with shape {height.shape}")
+
+    return height.astype('int16')  # type: ignore
--- bris_fiab/checkpoint/graph.py
+++ bris_fiab/checkpoint/graph.py
@@ -4,7 +4,7 @@ import earthkit.data as ekd
 from dataclasses import dataclass
 from .make_graph import build_stretched_graph
 from .update import update
-from .elevation import get_model_elevation
+from .elevation import get_model_elevation, get_model_elevation_mars_grid
 
 
 @dataclass
@@ -35,10 +35,17 @@ def run(topography_file: str | None, original_checkpoint: str, new_checkpoint: s
                 np.save(f, elevation)
 
     if add_model_elevation:
-        model_elevation = get_model_elevation(lat, lon)
+        # model_elevation = get_model_elevation(lat, lon)
+        if graph_config.area_latlon is not None:
+            model_elevation = get_model_elevation_mars_grid(
+                graph_config.area_latlon)
+        else:
+            model_elevation = get_model_elevation(lat, lon)
     else:
         model_elevation = None
 
+    print(f'DEBUG: exit now')
+    exit(1)
     graph = build_stretched_graph(
         lat.flatten(), lon.flatten(),
         global_grid='n320',
@@ -52,11 +59,12 @@ def run(topography_file: str | None, original_checkpoint: str, new_checkpoint: s
         torch.save(graph, save_graph_to)
         print('saved graph')
     # graph = torch.load(args.output, weights_only=False, map_location=torch.device('cpu'))
-    
-    update(graph, original_checkpoint, new_checkpoint, model_elevation, (lat, lon, elevation))
+
+    update(graph, original_checkpoint, new_checkpoint,
+           model_elevation, (lat, lon, elevation))
 
 
-def _get_latlon(topography_file: str) -> tuple[np.ndarray, np.ndarray, np.ndarray|None]:
+def _get_latlon(topography_file: str) -> tuple[np.ndarray, np.ndarray, np.ndarray | None]:
     topo = Topography.from_topography_file(topography_file)
     return topo.y_values, topo.x_values, topo.elevation
 
@@ -64,13 +72,13 @@ def _get_latlon(topography_file: str) -> tuple[np.ndarray, np.ndarray, np.ndarra
 def _get_lat_lon_from_area(area_latlon: tuple[float, float, float, float, float]) -> tuple[np.ndarray, np.ndarray]:
     """The function use earthkit.data to download lat/lon for the specified area from Mars.
     area_latlon: (north, west, south, east, resolution)
-    returns: lat, lon arrays
+    returns: lat, lon
     """
 
     # resolution is area_latlon[4]
     # return lat, lon arrays
     area = [area_latlon[0], area_latlon[1], area_latlon[2], area_latlon[3]]
-    ds = ekd.from_source(
+    ds = ekd.from_source(  # type: ignore
         'mars',
         {
             'AREA': area,
@@ -82,8 +90,10 @@ def _get_lat_lon_from_area(area_latlon: tuple[float, float, float, float, float]
             'levtype': 'sfc',
         }
     )
-    
-    lat, lon = ds[0].grid_points()
-    data = ds[0].to_numpy()
 
-    return lat.reshape(data.shape), lon.reshape(data.shape)
+    lat, lon = ds[0].grid_points()  # type: ignore
+    data = ds[0].to_numpy()  # type: ignore
+    print(
+        f"get_lat_lon_from_area: Downloaded lat/lon with shapes {lat.shape}, {lon.shape}, data shape {data.shape}")
+
+    return lat.reshape(data.shape), lon.reshape(data.shape)  # type: ignore
