9bfaf9a21618e5bb876e08d3f7a1009d0196f940
Vegard Bønes <vegardb@met.no>
2025-10-01 14:31:44 +0200

    create tests for adiabatic corrections

--- bris_fiab/anemoi_plugins/inference/apply_adiabatic_corrections/apply_adiabatic_corrections_test.py
+++ bris_fiab/anemoi_plugins/inference/apply_adiabatic_corrections/apply_adiabatic_corrections_test.py
@@ -7,72 +7,60 @@ import numpy as np
 
 from bris_fiab.anemoi_plugins.inference.apply_adiabatic_corrections.apply_adiabatic_corrections import AdiabaticCorrector
 
-ekd.config.set("cache-policy", "user")
+class AbstractAdiabaticCorrectorTest:
+    grib_file = 'none.grib'  # Override in subclasses
 
-
-class TestAdiabaticCorrector:
-
-    def _download_test_data(self) -> ekd.FieldList:
-        raw = ekd.from_source(
-            'mars',
-            {
-                'class': 'od',
-                'stream': 'oper',
-                'area': [2, 0, 0, 2],
-                'date': ['2025-03-05'],
-                'expver': '0001',
-                'levtype': 'sfc',
-                'grid': '1/1',
-                'param': ['10u', '10v', '2t', '2d', 'sp', 'z'],
-                'step': [0, 6],
-                'time': '0000',
-                'type': 'fc'
-            }
-        )
-        fake = []
-        for d in raw:
-            values = np.zeros_like(d.to_numpy())
-            if d.metadata('param') == '2t':
-                values = np.full(d.to_numpy().shape, fill_value=300)
-            elif d.metadata('param') == '2d':
-                values = np.full(d.to_numpy().shape, fill_value=290)
-            elif d.metadata('param') == 'sp':
-                values = np.full(d.to_numpy().shape, fill_value=1000)                
-            f = d.copy(values=values)
-            fake.append(f)
-
-        return ekd.SimpleFieldList(fake)
-
-    def _get_test_data(self) -> ekd.FieldList:
-        test_file = os.path.join(os.path.dirname(__file__), 'test_data.grib')
-        if not os.path.exists(test_file):
-            test_data = self._download_test_data()
-            test_data.to_target('file', test_file)
-        return ekd.from_source('file', test_file)
-
-    def test_apply(self):
-        test_data = self._get_test_data()
-        assert len(test_data) == 12 # type: ignore
-
-        z = test_data.sel(param='z')[0]
+    def setup_method(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'test_data', self.grib_file)
+        self.test_data = ekd.from_source('file', test_file)
+        z = self.test_data.sel(param='z')[0]
 
         model_elevation = np.zeros(z.shape) * units.meter
-        correct_elevation = np.full(z.shape, 1000) * units.meter
+        self.correct_elevation = np.full(z.shape, 1000) * units.meter
 
-        corrector = AdiabaticCorrector(model_elevation=model_elevation, correct_elevation=correct_elevation)
-        corrected = corrector.apply(test_data)
+        corrector = AdiabaticCorrector(
+            model_elevation=model_elevation, correct_elevation=self.correct_elevation)
 
-        assert len(corrected) == 12 # type: ignore
+        self.corrected = corrector.apply(self.test_data)  # type: ignore
+    
 
-        assert corrected.sel(param='2t', step=0)[0].to_numpy()[0,0] == 293.5
-        assert corrected.sel(param='2t', step=6)[0].to_numpy()[0,0] == 293.5
+class TestAdiabaticCorrectorVariableTimes(AbstractAdiabaticCorrectorTest):
+    grib_file = '1.grib'
+ 
+    def test_same_output_size(self):
+        assert len(self.corrected) == len(self.test_data)  # type: ignore
 
-        assert corrected.sel(param='2d', step=0)[0].to_numpy()[0,0] < 290
-        assert corrected.sel(param='2d', step=6)[0].to_numpy()[0,0] < 290
+    def test_2t(self):
+        original_t = self.test_data.sel(param='2t', levtype='sfc').to_numpy()
+        corrected_t = self.corrected.sel(param='2t', levtype='sfc').to_numpy()
+        np.allclose(corrected_t, original_t - 6.5)
 
-        assert corrected.sel(param='sp', step=0)[0].to_numpy()[0,0] < 1000
-        assert corrected.sel(param='sp', step=6)[0].to_numpy()[0,0] < 1000
+    def test_2d(self):
+        original_d = self.test_data.sel(param='2d', levtype='sfc').to_numpy()
+        corrected_d = self.corrected.sel(param='2d', levtype='sfc').to_numpy()
+        assert np.all(corrected_d < original_d)
+        corrected_t = self.corrected.sel(param='2t', levtype='sfc').to_numpy()
+        assert np.all(corrected_d < corrected_t)
 
-        expected_z = metpy.calc.height_to_geopotential(1000 * units.meter).magnitude
-        assert corrected.sel(param='z', step=0)[0].to_numpy()[0,0] == expected_z
-        assert corrected.sel(param='z', step=6)[0].to_numpy()[0,0] == expected_z
+    def test_sp(self):
+        original_sp = self.test_data.sel(param='sp', levtype='sfc').to_numpy()
+        corrected_sp = self.corrected.sel(param='sp', levtype='sfc').to_numpy()
+        assert np.all(corrected_sp < original_sp)
+
+    def test_others(self):
+        for n, d in enumerate(self.test_data):
+            if d.metadata('param') in ['2t', '2d', 'sp']:
+                # these are supposed to change
+                continue
+            original = d.to_numpy()
+            corrected = self.corrected[n].to_numpy()  # type: ignore
+            np.allclose(corrected, original)
+
+
+class TestAdiabaticCorrectorNonChangingTimes(AbstractAdiabaticCorrectorTest):
+    grib_file = '2.grib'
+
+    def test_z(self):
+        corrected_z = self.corrected.sel(param='z', levtype='sfc').to_numpy()
+        expected_z = metpy.calc.height_to_geopotential(self.correct_elevation).magnitude
+        assert np.allclose(corrected_z, expected_z)
