7a69bc06994ee7977880474ed601d60aa8a57439
Vegard Bønes <vegardb@met.no>
2025-10-07 13:42:36 +0200

    remove unused commands

--- bris_fiab/scripts/checkpoint/__init__.py
+++ bris_fiab/scripts/checkpoint/__init__.py
@@ -1,8 +1,6 @@
 import click
 from .move_domain import move_domain
 from .download_orography import download_orography
-from .update_metadata import update_metadata
-from .update_checkpoint import update_checkpoint
 
 
 @click.group()
@@ -13,5 +11,3 @@ def checkpoint():
 
 checkpoint.add_command(move_domain)
 checkpoint.add_command(download_orography)
-checkpoint.add_command(update_metadata)
-checkpoint.add_command(update_checkpoint)
