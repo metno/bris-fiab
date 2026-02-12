9d8226fa73c2c56f6612c15dc4cc98621564b5c5
Børge Moe <borge.moe@met.no>
2026-02-12 10:39:26 +0100

    Update

--- bris-adapt/bris_adapt/scripts/__init__.py
+++ bris-adapt/bris_adapt/scripts/__init__.py
@@ -2,7 +2,6 @@ import click
 
 from .checkpoint import checkpoint
 from .process import process
-from .run import run
 
 
 @click.group()
@@ -10,6 +9,5 @@ def cli():
     pass
 
 
-cli.add_command(run)
 cli.add_command(checkpoint)
 cli.add_command(process)
